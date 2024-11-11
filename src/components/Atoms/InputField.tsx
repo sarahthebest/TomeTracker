45628ecc9db5ce3e41e20726e5e2c99e1bac1abb
sarahthebest <sarah.emmoth@gmail.com>
2024-11-11 15:14:20 +0100

    collect and manage form state from parent component

--- src/components/Atoms/InputField.tsx
+++ src/components/Atoms/InputField.tsx
@@ -1,17 +1,32 @@
-type InputFieldProps = {
+export type InputFieldProps = {
     label: string;
     type: string;
     id: string;
-  };
-  
-  const InputField = ({ label, type, id }: InputFieldProps) => {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const InputField: React.FC<InputFieldProps> = ({
+    label,
+    type,
+    id,
+    value,
+    onChange,
+}) => {
     return (
-      <div className="input_wrapper flex justify-between">
-        <label className="text-lg" htmlFor={id}>{label}</label>
-        <input className="border p-1 w-4/5 rounded focus:outline-none" type={type} id={id} />
-      </div>
+        <div className="input_wrapper flex justify-between">
+            <label className="text-lg" htmlFor={id}>
+                {label}
+            </label>
+            <input
+                className="border p-1 w-4/5 rounded focus:outline-none"
+                type={type}
+                id={id}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
     );
-  };
-  
-  export default InputField;
-  
+};
+
+export default InputField;
