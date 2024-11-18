c4acee3853f24171509668c31020d018bfa91386
sarahthebest <sarah.emmoth@gmail.com>
2024-11-18 17:37:01 +0100

    Install react-icons, add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#A3B18A',
+        secondary: '#588157',
+        bg: '#344E41',
+        border: 'rgba(254, 253, 255, 0.495)',
+        text: '#DAD7CD',
+      },
+    },
   },
   plugins: [],
 }
