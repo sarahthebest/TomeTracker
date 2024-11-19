6b45d1efcba628628aeafb2b22b264ed847d69ee
sarahthebest <sarah.emmoth@gmail.com>
2024-11-19 12:03:20 +0100

    Header has featureboxes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,33 @@
-import Btn from "../Atoms/Btn";
-import FeaturesList from "./FeatureBok/FeatureBox";
+import { Link } from "react-router-dom";
+import FeaturesList from "./FeatureBox/FeatureBox";
 import "./Header.css";
 import { BsStars } from "react-icons/bs";
 
 const Header = () => {
     return (
         <>
-            <div className="header px-4 flex flex-col gap-2 mb-4 p-4 absolute pt-20">
+            <div className="header px-4 flex flex-col gap-4 mb-4 p-4 absolute pt-20">
                 <div className="headerContent flex flex-row place-content-center">
                     <img
                         className="w-1/4"
                         src="src/assets/reading.svg"
                         alt="Illustration from undraw.co"
                     />
-                    <div className="content w-2/5">
-                        <h1 className="huge medium font-bold">TomeTracker</h1>
-                        <p className="light mb-4">
+                    <div className="content w-2/5 ">
+                        <h1 className="huge medium font-bold text-accent">
+                            TomeTracker
+                        </h1>
+                        <p className="light mb-4 ">
                             Wander through stories, find magic in every page,
                             and let each book lead you to new adventures,
                             wrapped in the warmth of timeless tales.
                         </p>
-                        <Btn
-                            onClick={function (): void {
-                                throw new Error("Function not implemented.");
-                            }}
-                            text={"Join Now"}
-                            icon={<BsStars />}
-                        />
+                        <Link
+                            to={"/login"}
+                            className="flex gap-2 hover:brightness-90 place-items-center cartoon-shadow text-gray-900 bg-primary w-fit rounded p-2"
+                        >
+                            Join now <BsStars color="var(--accent)"/>
+                        </Link>
                     </div>
                 </div>
                 <FeaturesList />
