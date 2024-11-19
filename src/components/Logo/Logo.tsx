ffc3abdfeeb7efdb5250091bcb09d3c4e1e04950
sarahthebest <sarah.emmoth@gmail.com>
2024-11-19 12:05:38 +0100

    Logo links to "/" remove addBook btn from nav

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Logo = () => {
     return (
-        <div className="logo_wrapper w-24 h-full rounded-e-full pe-2">
+        <Link
+            to={"/"}
+            className="logo_wrapper w-60 h-full group rounded-e-full pe-2 flex items-center"
+        >
             <img
                 src="src/assets/logo.png"
-                className="h-fit w-16 ms-3  mt-2 hover:invert duration-200"
+                className="h-fit w-16 ms-3 mt-2 duration-200 group-hover:invert"
                 alt="Logo image"
             />
-        </div>
+            <h1 className="medium text-gray-900 duration-200 group-hover:invert">
+                TomeTracker
+            </h1>
+        </Link>
     );
 };
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import AddBook from "../AddBook/AddBook";
+import { GiBookshelf } from "react-icons/gi";
 import Btn from "../Atoms/Btn";
 import Logo from "../Logo/Logo";
 import "./Navbar.css";
@@ -12,11 +12,16 @@ const Navbar = () => {
 
     return (
         <div className="nav w-full flex border-b border-border/20 flex-row mx-auto
-        ' justify-between sticky top-0 py-2 z-10">
+        ' justify-between sticky top-0 py-2 z-50">
             <Logo />
             <div className="user_handler flex flex-row gap-4 px-4 h-10 my-auto z-10">
                 <div className="search"></div>
-                <AddBook />
+                <Btn
+                    onClick={() => handleNavigate("/shelves")}
+                    text="Shelves"
+                    backgroundColor="var(--primary)"
+                    icon={<GiBookshelf />}
+                />
                 <Btn
                     onClick={() => handleNavigate("/login")}
                     text="Login"
