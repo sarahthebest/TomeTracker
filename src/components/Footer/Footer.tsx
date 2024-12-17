de451b49811d65d6bde0cf7358362d41001d0f65
sarahthebest <sarah.emmoth@gmail.com>
2024-12-17 14:53:35 +0100

    add dummy links to footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,35 @@
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <footer className="bg-primary p-10 flex flex-col relative justify-between">
+            <div className="footer_content flex gap-20">
+                <div className="about flex flex-col gap-4">
+                    <h3 className="heading text-2xl">About</h3>
+                    <p>About TomeTracker</p>
+                    <p>Contact</p>
+                </div>
+                <div className="user flex flex-col gap-4">
+                    <h3 className="heading text-2xl">User</h3>
+                    <p>Register</p>
+                    <p>Shelves</p>
+                </div>
+                <div className="socials flex flex-col gap-4">
+                    <h3 className="heading text-2xl">Socials</h3>
+                    <a
+                        href="https://github.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex gap-2 items-center"
+                    >
+                        <FaGithub size={20} />
+                        GitHub
+                    </a>
+                </div>
+            </div>
+            <h2 className="text-4xl heading mx-auto">TomeTracker</h2>
+        </footer>
+    );
+};
+
+export default Footer;
