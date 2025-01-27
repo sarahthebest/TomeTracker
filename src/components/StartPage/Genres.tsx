3677935d35faf89623e3b10ea873d9d8814a293d
sarahthebest <sarah.emmoth@proton.me>
2025-01-27 16:18:28 +0100

    Genrebox styling changes, genre images added

--- src/components/StartPage/Genres.tsx
+++ src/components/StartPage/Genres.tsx
@@ -1,25 +1,52 @@
 const Genres = () => {
     const genres = [
-        "Romance 💃",
-        "Fantasy 🐉",
-        "Sci-Fi 🤖",
-        "Young Adult ✨",
-        "Mystery 🕵️",
-        "Horror 👹",
+        {
+            genre: "Romance",
+            coverImg: "src/assets/img/genreIMGs/romance.jpg",
+        },
+        {
+            genre: "Fantasy",
+
+            coverImg: "src/assets/img/genreIMGs/fantasy.jpg",
+        },
+        {
+            genre: "Sci-Fi",
+            coverImg: "src/assets/img/genreIMGs/scifi.jpg",
+        },
+        {
+            genre: "Young Adult",
+            coverImg: "src/assets/img/genreIMGs/youngadult.webp",
+        },
+        {
+            genre: "Mystery",
+            coverImg: "src/assets/img/genreIMGs/mystery.jpg",
+        },
+        {
+            genre: "Horror",
+            coverImg: "src/assets/img/genreIMGs/horror.jpg",
+        },
     ];
     return (
-        <div className="genres md:w-2/3 mx-auto w-fit overflow-hidden">
+        <div className="genres w-full md:w-2/3 mx-auto overflow-hidden">
             <h2 className="mt-4 mb-6">
-                Check out some popular <span className="underline">Genres</span>
+                Check out some our popular{" "}
+                <span className="underline italic">Genres</span>
             </h2>
-            <div className="genreBoxes w-96 md:w-full gap-4 flex-row rounded overflow-x-auto whitespace-nowrap pb-2">
+            <div className="genreBoxes md:w-full gap-4 flex-row rounded overflow-x-auto whitespace-nowrap pb-2">
                 {genres.map((genre, index) => (
                     <div
-                        className="genre p-4 justify-between overflow-y-hidden rounded hover:brightness-125 ease-in-out transition-all duration-200
-                         bg-gradient-to-t from-bg to-primary"
+                        className="genre inline p-4 justify-between relative overflow-hidden rounded ease-in-out transition-all duration-200
+                         bg-gradient-to-t from-bg to-primary align-bottom"
                         key={index}
                     >
-                        <h3 className="genreTitle">{genre}</h3>
+                        <img
+                            src={genre.coverImg}
+                            className="absolute z-10 left-0 -top-10 object-fill w-full"
+                        />
+                        <h3 className="genreTitle z-50 relative text-text">
+                            {genre.genre}
+                        </h3>
+                        <div className="progBlurLeft rounded absolute left-0 top-0 z-40" />
                     </div>
                 ))}
             </div>
