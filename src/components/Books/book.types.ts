608f5d0a2b0a944d60b8c1cef445a54dbe6473de
sarahthebest <sarah.emmoth@proton.me>
2025-02-05 14:37:05 +0100

    Remove duplicate volumeinfo containing same types for book.

--- src/components/Books/book.types.ts
+++ src/components/Books/book.types.ts
@@ -1,39 +1,27 @@
-import { BookStatus } from "../../types/global.types";
 
 export interface ImageLinks {
     smallThumbnail?: string;
     thumbnail?: string;
 }
 
-export interface VolumeInfo {
-    title: string;
-    authors: string[];
-    description?: string;
-    publisher?: string;
-    publishedDate?: string;
-    pageCount?: number;
-    categories?: string[];
-    imageLinks?: ImageLinks;
-}
-
 export interface Book {
     _id: string;
-    volumeInfo: VolumeInfo;
-    title: string; 
-    authors: string[]; 
-    categories?: string[]; 
-    averageRating?: number; 
-    isbn?:string;
-    pageCount?: number; 
-    publishedDate?: string; 
-    description?: string; 
-    imageLinks?: ImageLinks; 
-    status: BookStatus; 
+    title: string;
+    authors: string[];
+    categories?: string[];
+    averageRating?: number;
+    isbn?: string;
+    pageCount?: number;
+    publishedDate?: string;
+    description?: string;
+    imageLinks?: ImageLinks;
+    status: BookStatus;
+}
 
-    
-};
 export const getBookImage = (imageLinks?: ImageLinks) => {
     const imgNotFound = "src/assets/img/No_Cover.webp"; 
     
     return imageLinks?.thumbnail || imgNotFound;
 };
+
+export type BookStatus = "Want to read" | "Reading" | "Completed";
