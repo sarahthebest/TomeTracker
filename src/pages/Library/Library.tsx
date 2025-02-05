2401f7f05f3aa7855ca2252b96a98169818527b1
sarahthebest <sarah.emmoth@proton.me>
2025-02-05 16:53:06 +0100

    more content for library page

--- src/pages/Library/Library.tsx
+++ src/pages/Library/Library.tsx
@@ -10,6 +10,14 @@ const Library = () => {
             <Bg />
             <Flex className="mx-auto md:w-2/3 bg-red-950 z-50 mt-20">
                 <h2 className="text-2xl">Popular books</h2>
+                <div className="flex flex-row"></div>
+            </Flex>
+            <Flex className="mx-auto md:w-2/3 bg-red-950 z-50 mt-20">
+                <h2 className="text-2xl">Romance books</h2>
+                <div className="flex flex-row"></div>
+            </Flex>            <Flex className="mx-auto md:w-2/3 bg-red-950 z-50 mt-20">
+                <h2 className="text-2xl">Fantasy books</h2>
+                <div className="flex flex-row"></div>
             </Flex>
             <CookieConsent />
         </div>
