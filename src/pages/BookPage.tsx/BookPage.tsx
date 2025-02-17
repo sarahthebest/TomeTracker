59f9a87dcf31b13a94971f2ebfe72ffa5dc8b377
sarahthebest <sarah.emmoth@proton.me>
2025-02-17 15:15:43 +0100

    Conditionally render show more/less btn in book page based on description length.

--- src/pages/BookPage.tsx/BookPage.tsx
+++ src/pages/BookPage.tsx/BookPage.tsx
@@ -37,64 +37,74 @@ const BookPage = () => {
     return (
         <>
             <Bg />
-        <section className="page overflow-hidden overflow-x-hidden flex flex-col relative min-h-screen">
-            <Navbar />
-            <CookieConsent />
-            <div className="book_wrapper flex flex-col gap-4 relative px-4 md:px-0 z-10 mt-20 md:w-2/3 mx-auto">
-                <Link
-                    to="/shelves"
-                    className="flex flex-row place-items-center gap-2"
-                >
-                    <IoReturnUpBackOutline size={20} /> Return
-                </Link>
-                <div className="book_info flex flex-col md:flex-row justify-between md:place-items-start">
-                    <Flex className="md:gap-8 gap-2 mb-4 md:mb-0 flex-col md:flex-row md:place-items-center">
-                        <img
-                            src={thumbnailUrl}
-                            alt={`Thumbnail for ${book.title}`}
-                            className="w-fit"
-                        />
-                        <Flex vertical gap={8} className="md:text-start">
-                            <h1 className="heading text-4xl">{book.title}</h1>
-                            <p>
-                                Written by {""}
-                                <span className="text-accent">
-                                    {book.authors?.join(", ") ||
-                                        "Unknown Author"}
-                                </span>
-                                <br />
-                                Published {book.publishedDate || "Unknown Date"}
-                                <br />
-                                {book.pageCount || "N/A"} pages
-                            </p>
-                            <Tag color="magenta" className="w-fit">
-                                {book.categories?.join(", ") || "Uncategorized"}
-                            </Tag>
+            <section className="page overflow-hidden overflow-x-hidden flex flex-col relative min-h-screen">
+                <Navbar />
+                <CookieConsent />
+                <div className="book_wrapper flex flex-col gap-4 relative px-4 md:px-0 z-10 mt-20 md:w-2/3 mx-auto">
+                    <Link
+                        to="/shelves"
+                        className="flex flex-row place-items-center gap-2"
+                    >
+                        <IoReturnUpBackOutline size={20} /> Return
+                    </Link>
+                    <div className="book_info flex flex-col md:flex-row justify-between md:place-items-start">
+                        <Flex className="md:gap-8 gap-2 mb-4 md:mb-0 flex-col md:flex-row md:place-items-center">
+                            <img
+                                src={thumbnailUrl}
+                                alt={`Thumbnail for ${book.title}`}
+                                className="w-fit"
+                            />
+                            <Flex vertical gap={8} className="md:text-start">
+                                <h1 className="heading text-4xl">
+                                    {book.title}
+                                </h1>
+                                <p>
+                                    Written by {""}
+                                    <span className="text-accent">
+                                        {book.authors?.join(", ") ||
+                                            "Unknown Author"}
+                                    </span>
+                                    <br />
+                                    Published{" "}
+                                    {book.publishedDate || "Unknown Date"}
+                                    <br />
+                                    {book.pageCount || "N/A"} pages
+                                </p>
+                                <Tag color="magenta" className="w-fit">
+                                    {book.categories?.join(", ") ||
+                                        "Uncategorized"}
+                                </Tag>
+                            </Flex>
                         </Flex>
+                        <AddToShelf book={book} />
+                    </div>
+                    {book.description ? (
+                        <Flex vertical>
+                            <p>
+                                {book.description.length > 500
+                                    ? collapsedText
+                                        ? book.description
+                                        : limitCharacters(book.description, 500)
+                                    : book.description}
+                            </p>
+                            {book.description.length > 500 && (
+                                <button
+                                    className="hover:bg-pop/10 w-full py-1 rounded-xl mt-4"
+                                    onClick={() =>
+                                        setCollapsedText(!collapsedText)
+                                    }
+                                >
+                                    {collapsedText ? "Show Less" : "Read More"}
+                                </button>
+                            )}
+                        </Flex>
+                    ) : null}
+                    <Flex justify="space-between" vertical>
+                        <BookStoreLinks book={book} />
+                        <MoreBooksByAuthor book={book} />
                     </Flex>
-                    <AddToShelf book={book} />
                 </div>
-                {book.description ? (
-                    <Flex vertical>
-                        <p>
-                            {collapsedText
-                                ? book.description
-                                : limitCharacters(book.description, 500)}
-                        </p>
-                        <button
-                            className="hover:bg-pop/10 w-full py-1 rounded-xl mt-4"
-                            onClick={() => setCollapsedText(!collapsedText)}
-                        >
-                            {collapsedText ? "Show Less" : "Read More"}
-                        </button>
-                    </Flex>
-                ) : null}
-                <Flex justify="space-between" vertical>
-                    <BookStoreLinks book={book} />
-                    <MoreBooksByAuthor book={book} />
-                </Flex>
-            </div>
-        </section>
+            </section>
         </>
     );
 };
